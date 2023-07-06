00cd590e9bc84210d27ad2088717770aea2a7173
PrasannaSagar1 <prasanna.glansa@gmail.com>
2023-07-06 11:01:34 +0530

    Add project files.

---
+++ FirstAPI/Controllers/SampleController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Http;
+using System.Net;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : Controller
+    {
+
+        SqlConnection Con = new SqlConnection(@"server=DESKTOP-9R4PCGG\SQLEXPRESS;initial catalog=AndonDB;Integrated Security=true" providerName = "System.Data.SqlClient"
+
+
+        SqlCommand Cmd;
+        SqlDataReader DR;
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Sample/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sample/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sample/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sample/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sample/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sample/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FirstAPI/Models/UDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FirstAPI.Models
+{
+    public class UDashboard
+    {
+        public int PId { get; set; }
+        public int Jig_ID { get; set; }
+        public string Jig_Name { get; set; }
+        public string Level_Name { get; set; }
+        public string issue { get; set; }      
+        public DateTime Fromdate { get; set; }
+        public int Aging { get; set; }
+        public int Hour { get; set; }
+    }
+}
